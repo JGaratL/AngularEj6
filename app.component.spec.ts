e6756d8e8d13fe677d95bcff6244efa1c5581062
JGaratL <120282132+JGaratL@users.noreply.github.com>
2023-06-18 23:59:39 +0200

    Update app.component.spec.ts

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NO_ERRORS_SCHEMA } from "@angular/core"
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+
+import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        FormsModule,
+        MatButtonModule,
+        ReactiveFormsModule,
       ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
@@ -34,9 +51,7 @@ describe('AppComponent', () => {
   it('firstValue equal 0'), () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.firstNumber)
-      .withContext('At start')
-      .toEqual(0);
+    expect(app.firstNumber).toEqual(0);
   }
 
   it('secondValue equal 0'), () => {
@@ -46,4 +61,14 @@ describe('AppComponent', () => {
       .withContext('At start')
       .toEqual(0);
   }
+
+  it('result equal 0'), () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.result)
+      .withContext('At start')
+      .toEqual(0);
+  }
+
+  
 });
