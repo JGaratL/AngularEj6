0a5aac8f5e2244f2418ae84b2a945c612c42b364
JGaratL <120282132+JGaratL@users.noreply.github.com>
2023-06-18 23:46:46 +0200

    Create app.component.ts

---
+++ app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'calculator';
+  firstNumber: number = 0;
+  secondNumber: number = 0;
+  result: number = 0;
+
+
+  adition() {
+    this.result = this.firstNumber + this.secondNumber;
+  };
+
+  subtraction() {    
+    this.result = this.firstNumber - this.secondNumber;
+  };
+
+  multiply() {
+    this.result = this.firstNumber * this.secondNumber;
+  };
+
+  reset() {
+    this.firstNumber = 0;
+    this.secondNumber = 0;
+    this.result = 0;
+  }
+}
